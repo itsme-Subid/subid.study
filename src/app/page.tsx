af9bdef15b2b36e530380b756063bff55e948b2b
SUBID DAS <subid.das@awesomesuite.com>
2024-02-18 03:53:25 +0530

    Update page styling to use white text on a dark background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => (
-  <main className="flex items-center justify-center w-screen h-screen bg-gray-100">
+  <main className="flex items-center justify-center w-screen h-screen bg-gray-950">
     <div className="text-center">
-      <h1 className="text-4xl font-bold text-gray-900">Padhle bhai, padhle</h1>
-      <h2 className="text-2xl font-semibold text-gray-700">
+      <h1 className="text-4xl font-bold text-white">Padhle bhai, padhle</h1>
+      <h2 className="text-2xl font-semibold text-white/60">
         Babu sona karne se ghar nahi chalta
       </h2>
     </div>
